6cfc0cd5216cd46a0cf226dfbc5ef2fd694e8866
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-09-15 22:32:24 +0700

    #33 | Fix some bug

---
+++ BingNew.BusinessLogicLayer/Services/Common/DataSourceFactory.cs
@@ -0,0 +1,32 @@
+﻿using BingNew.BusinessLogicLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingNew.BusinessLogicLayer.Services.Common
+{
+    // Factory Method Design Pattern
+    public class DataSourceFactory
+    {
+        public enum DataSource
+        {
+            ApiDataSource,
+            RssDataSource
+        }
+
+        public static IDataSource CreateDataSource(DataSource dataSource)
+        {
+            IDataSource data = null;
+            switch (dataSource)
+            {
+                case DataSource.ApiDataSource:
+                    return new ApiDataSource();
+                case DataSource.RssDataSource:
+                    return new RssDataSource(); 
+            }
+            return data;
+        }
+    }
+}
