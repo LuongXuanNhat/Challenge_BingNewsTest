3d083860cdf7b3b15b1487e2d471f5aa4e662b0a
LuongXuanNhat <xuannhat1932002@gmail.com>
2023-09-22 01:49:58 +0700

    #39 | Update test case for QueryFirst<T> method

--- NewsAggregationTest/ORMTest.cs
+++ NewsAggregationTest/ORMTest.cs
@@ -1,7 +1,5 @@
 ﻿using BingNew.DataAccessLayer.Constants;
 using BingNew.DataAccessLayer.Models;
-using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Reflection;
@@ -37,17 +35,81 @@ namespace NewsAggregationTest
                 var sql = "SELECT * FROM Article where Id = 'dfdab4e6-538b-4ef5-8b69-00f99a9ad6bf'";
                 var result = connection.QuerySingle(sql);
                 Assert.NotNull(result);
+               //// Assert.IsType<Article>(result); - Error
             }
         }
         [Fact]
-        public void GetArticle_Error_Return_Null1()
+        public void GetArticle_Return_Null1()
         {
             using (var connection = new SqlConnection(_connecString))
             {
                 connection.Open();
-                var sql = "SELECT * FROM Article where Id = 'dfdab4e6-538b-4ef5-8b69-00f99a9ad6bf'";
+                var sql = "SELECT * FROM Article where Id = 'dfdab4e6-538b-4ef5-8b69-00f99a9ad6bb'";
                 var result = connection.QuerySingleOrDefault(sql);
+                Assert.Null(result);
+            }
+        }
+        [Fact]
+        public void GetArticle_Return_Null2()
+        {
+            using (var connection = new SqlConnection(_connecString))
+            {
+                connection.Open();
+                var sql = "SELECT * FROM Article where Id = 'dfdab4e6-538b-4ef5-8b69-00f99a9ad6bb'";
+                var result = connection.QuerySingleOrDefault<Article>(sql);
+                Assert.Null(result);
+            }
+        }
+        [Fact]
+        public void GetArticle_Error_Return_Exception_When_More_Than_One_Element_1()
+        {
+            using (var connection = new SqlConnection(_connecString))
+            {
+                connection.Open();
+                var sql = "SELECT * FROM Article where ProviderId = 2";
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    connection.QuerySingleOrDefault<Article>(sql);
+                });
+            }
+        }
+        [Fact]
+        public void GetArticle_Error_Return_Exception_When_More_Than_One_Element_2()
+        {
+            using (var connection = new SqlConnection(_connecString))
+            {
+                connection.Open();
+                var sql = "SELECT * FROM Article where ProviderId = 2";
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    connection.QuerySingleOrDefault(sql);
+                });
+            }
+        }
+        [Fact]
+        public void GetArticleFirst_Success()
+        {
+            using (var connection = new SqlConnection(_connecString))
+            {
+                connection.Open();
+                var sql = "SELECT * FROM Article where ProviderId = 2";
+                var result = connection.QueryFirst<Article>(sql);
+
                 Assert.NotNull(result);
+                Assert.IsType<Article>(result);
+            }
+        }
+        [Fact]
+        public void GetArticleFirst_Error_When_Not_Found()
+        {
+            using (var connection = new SqlConnection(_connecString))
+            {
+                connection.Open();
+                var sql = "SELECT * FROM Article where ProviderId = 1";
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    connection.QueryFirst<Article>(sql);
+                });
             }
         }
 
@@ -76,6 +138,57 @@ namespace NewsAggregationTest
                 }
             }
         }
+        public static T QuerySingleOrDefault<T>(this SqlConnection connection, string sql, int? commandTimeout = null,SqlParameter[] sqlParameters = null, IDbTransaction transaction = null)
+            where T : class, new()
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = sql;
+                command.CommandType = CommandType.Text;
+                command.CommandTimeout = commandTimeout ?? 30;
+
+                T result = null; 
+                bool hasResult = false; // Biến để kiểm tra có dữ liệu hay không
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (hasResult)
+                        {
+                            throw new InvalidOperationException("Multiple rows found");
+                        }
+                        result = ConvertToObject<T>(reader);
+                        hasResult = true; 
+                    }
+                }
+                if (!hasResult)
+                {
+                    return null;
+                }
+                return result;
+            }
+        }
+        public static T QueryFirst<T>(this SqlConnection connection, string sql, int? commandTimeout = null,SqlParameter[] sqlParameters = null, IDbTransaction transaction = null)
+            where T : class, new()
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.Connection = connection;
+                command.CommandText = sql;
+                command.CommandType = CommandType.Text;
+                command.CommandTimeout = commandTimeout ?? 30;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return ConvertToObject<T>(reader);
+                    }
+                }
+                throw new InvalidOperationException("Invalid return data: zero element");
+            }
+        }
 
         public static dynamic QuerySingle(this SqlConnection connectionn, string sql)
         {
@@ -83,25 +196,28 @@ namespace NewsAggregationTest
             {
                 using (var reader = command.ExecuteReader())
                 {
-                    if (reader.Read())
+                    dynamic result = null;
+                    int rowCount = 0;
+                    while (reader.Read())
                     {
-                        // các thuộc tính và giá trị vào đối tượng này mà không cần biết trước kiểu dữ liệu cụ thể của các thuộc tính.
-                        dynamic result = new System.Dynamic.ExpandoObject();
-                        string tableName = GetTypeNameFromSql(sql);
-                        Type classType = Type.GetType("Namespace." + tableName);
+                        rowCount++;
+                        if (rowCount > 1)
+                        {
+                            throw new InvalidOperationException("Invalid return data: zero or more than one element");
+                        }
+                        result = new System.Dynamic.ExpandoObject();
                         for (int i = 0; i < reader.FieldCount; i++)
                         {
                             var columnName = reader.GetName(i);
                             ((IDictionary<string, object>)result)[columnName] = reader[i];
                         }
-
-                        return result;
                     }
-                    else
+
+                    if (rowCount == 0)
                     {
-                        return null;
                         throw new InvalidOperationException("Invalid return data: zero or more than one element");
                     }
+                    return result;
                 }
             }
         }
@@ -111,24 +227,28 @@ namespace NewsAggregationTest
             {
                 using (var reader = command.ExecuteReader())
                 {
-                    if (reader.Read())
+                    dynamic result = null;
+                    int rowCount = 0; 
+                    while (reader.Read())
                     {
-                        // các thuộc tính và giá trị vào đối tượng này mà không cần biết trước kiểu dữ liệu cụ thể của các thuộc tính.
-                        dynamic result = new System.Dynamic.ExpandoObject();
-                        string tableName = GetTypeNameFromSql(sql);
-                        Type classType = Type.GetType("Namespace." + tableName);
+                        rowCount++;
+                        if (rowCount > 1)
+                        {
+                            throw new InvalidOperationException("Invalid return data: more than one element");
+                        }
+                        result = new System.Dynamic.ExpandoObject();
                         for (int i = 0; i < reader.FieldCount; i++)
                         {
                             var columnName = reader.GetName(i);
                             ((IDictionary<string, object>)result)[columnName] = reader[i];
                         }
+                    }
 
-                        return result;
-                    }
-                    else
+                    if (rowCount == 0)
                     {
-                        throw new InvalidOperationException("Invalid return data: zero or more than one element");
+                        return null;
                     }
+                    return result;
                 }
             }
         }
